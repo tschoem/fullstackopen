fc384234f3c51e892adba5356428aca71d461727
Thomas Schoemaecker <thomas@smartly.io>
2022-04-11 17:43:12 +0100

    courseinfo step 3
    use of Objects

--- part1/courseinfo/README.md
+++ part1/courseinfo/README.md
@@ -1,17 +1,27 @@
-# 1.2: course information, step2
+# 1.3: course information step3
 
-<strong>Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders three Part components of which each renders the name and number of exercises of one part.</strong>
+Let's move forward to using objects in our application. Modify the variable definitions of the App component as follows and also refactor the application so that it still works:
 
 ```
-const Content = ... {
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
-      <Part .../>
-      <Part .../>
-      <Part .../>
+      ...
     </div>
   )
 }
 ```
-
-Our application passes on information in quite a primitive way at the moment, since it is based on individual variables. This situation will improve soon.
--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,15 +1,12 @@
 const Header = (props) => {
-  return (
-    <h1>
-      {props.course}
-    </h1>
-  )
+  console.log(props)
+  return <h1>{props.course}</h1>
 }
 
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -17,9 +14,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercise1} />
-      <Part part={props.part2} exercise={props.exercise2} />
-      <Part part={props.part3} exercise={props.exercise3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
   )
 }
@@ -34,18 +31,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
