065dd588b5e47bb10efb7e93445259ff5a7db06a
Thomas Schoemaecker <tdub@schoem.org>
2024-01-07 20:08:57 +0000

    Unicafe 1.6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,56 +1,88 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    setCounter(counter + 1)
-    console.log('increasing, value before', counter)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
-
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
   return (
     <div>
-      <Display counter={counter}/>
-
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
+/*const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)*/
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+/*const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    debugger
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight + 1)
+
+    setTotal(left + updatedRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
+
+      <p>total {total}</p>
+    </div>
+  )
+}*/
+
 export default App
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+//import './App.css'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1) 
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1) 
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1) 
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleClickGood} text='good' />
+      <Button handleClick={handleClickNeutral} text='neutral' />
+      <Button handleClick={handleClickBad} text='bad' />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
