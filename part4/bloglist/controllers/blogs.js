378506a8d8972050c17efce108da921225cc26d7
Thomas Schoemaecker <tdub@schoem.org>
2024-02-20 18:31:39 +0000

    4.19: Blog List Expansion, step 7

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,30 +1,44 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogList = await Blog.find({}).populate('user',{blogs: 0})
+  const blogList = await Blog.find({}).populate('user', { blogs: 0 })
   response.json(blogList)
 })
 
-blogsRouter.post('/', async (request, response ) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const usersInDb = await User.find({})
-  if (usersInDb.length > 0) {
-    const blogEntry = new Blog({
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-      title: body.title,
-      user: usersInDb[0].id
-    })
-    const savedBlog = await blogEntry.save()
-    usersInDb[0].blogs = usersInDb[0].blogs.concat(savedBlog._id)
-    await usersInDb[0].save()
-    response.status(201).json(savedBlog)
-  } else {
-    response.status(500).send({ error: 'could not find suitable user' })
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
+  const user = await User.findById(decodedToken.id)
+
+  //if (usersInDb.length > 0) {
+  const blogEntry = new Blog({
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    title: body.title,
+    user: user.id
+  })
+  const savedBlog = await blogEntry.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog)
+  //} else {
+  //  response.status(500).send({ error: 'could not find suitable user' })
+  //}
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
