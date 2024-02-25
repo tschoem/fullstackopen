cd026cfc49259b702e47412ec225a75cc579d769
Thomas Schoemaecker <tdub@schoem.org>
2024-02-25 11:26:57 +0000

    5.1: Blog List Frontend, step 1

--- part2/part2-1/src/App.jsx
+++ part2/part2-1/src/App.jsx
@@ -8,9 +8,11 @@ const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
-  ) 
+  )
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     console.log('effect')
@@ -20,12 +22,12 @@ const App = (props) => {
         setNotes(initialNotes)
       })
   }, [])
-  
+
   console.log('render', notes.length, 'notes')
 
   const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important === true)
+    ? notes
+    : notes.filter(note => note.important === true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -34,20 +36,20 @@ const App = (props) => {
       important: Math.random() < 0.5,
       //id: notes.length + 1,
     }
-  
+
     noteService
       .create(noteObject)
       .then(newNoteResponse => {
         setNotes(notes.concat(newNoteResponse))
         setNewNote('')
-    })
+      })
 
   }
 
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     noteService
       .update(id, changedNote)
       .then(updatedNote => {
@@ -69,32 +71,58 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map(note =>
           <Note
-          key={note.id}
-          note={note} 
-          toggleImportance={() => toggleImportanceOf(note.id)}
-        />
-      )}
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
       </ul>
       <form onSubmit={addNote}>
-        <input 
+        <input
           value={newNote}
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
       </form>
-      <Footer />   
+      <Footer />
     </div>
   )
 }
---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/Login'
+import Notification from './components/Notification'
+
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <h2>login to application</h2>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  return (
+    <div>
+      <Notification message={errorMessage} />
+      {user === null ?
+        loginForm() :
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
