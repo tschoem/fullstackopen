721717bcc9ebed89ed905b504a7af014c5327b3c
Thomas Schoemaecker <tdub@schoem.org>
2024-03-03 15:42:26 +0000

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,48 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailedView, setDetailedView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleView = async (event) => {
+    setDetailedView(!detailedView)
+  }
+
+  const showSummary = () => (
+    <>
+      {blog.title} {blog.author} <button onClick={toggleView}>show</button>
+    </>
+  )
+
+  const showDetails = () => (
+    <>
+      {blog.title} {blog.author} <button onClick={toggleView}>hide</button><br />
+      {blog.url}<br />
+      likes: {blog.likes} <button>like</button><br />
+      {blog.user.name}
+    </>
+  )
+
+  const renderBlog = () => {
+    console.log(blog)
+    if (detailedView) {
+      return showDetails()
+    } else {
+      return showSummary()
+    }
+  }
+  return (
+    <div style={blogStyle}>
+      {renderBlog()}
+    </div>
+  )
+}
 
 export default Blog
