d449a356710f1b2bc26781e464302f099cee1d67
Thomas Schoemaecker <tdub@schoem.org>
2024-03-03 17:34:48 +0000

    5.10: Blog List Frontend, step 10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -153,7 +153,7 @@ const App = () => {
           <Togglable buttonLabel="new note" ref={blogFormRef}>
             <NewBlogForm createBlog={createBlog} />
           </Togglable>
-          {blogs.map(blog =>
+          {blogs.sort((blog1, blog2) => blog2.likes - blog1.likes).map(blog =>
             <Blog key={blog.id} blog={blog} incrementLikes={() => incrementLikes(blog.id)} />
           )}
         </div>
