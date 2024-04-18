7e4bcde398e00c9ca3e49c3e9ec1179f45c19cff
Thomas Schoemaecker <tdub@schoem.org>
2024-04-18 09:28:56 +0100

    5.18: Blog List End To End Testing, step 2

--- part5/bloglist-frontend/e2e/tests/bloglist.spec.js
+++ part5/bloglist-frontend/e2e/tests/bloglist.spec.js
@@ -1,7 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { loginWith, createBlog } from './helper'
 
 test.describe('Blog app', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Thomas Schoemaecker',
+        username: 'tom',
+        password: 'password'
+      }
+    })
+
     await page.goto('/')
   })
 
@@ -9,5 +19,21 @@ test.describe('Blog app', () => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  test.describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, "tom", "password")
+      await expect(page.getByText('Thomas Schoemaecker logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, "tom", "wrong password")
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
   })
 })
