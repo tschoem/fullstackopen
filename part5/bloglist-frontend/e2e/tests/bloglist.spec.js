04a9d8c5f2d4c631979f768307d8ffba24859662
Thomas Schoemaecker <tdub@schoem.org>
2024-04-18 23:13:14 +0100

    5.23: Blog List End To End Testing, step 7

--- part5/bloglist-frontend/e2e/tests/bloglist.spec.js
+++ part5/bloglist-frontend/e2e/tests/bloglist.spec.js
@@ -61,10 +61,12 @@ test.describe('Blog app', () => {
       await createBlog(page, 'React patterns', 'Michael Chan', 'https://reactpatterns.com/')
       await createBlog(page, 'Canonical string reduction', 'Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html')
 
+      // blog can be expanded with show button
       await expect(page.locator('.blog-summary').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'show' })).toBeVisible()
       await page.locator('.blog-summary').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'show' }).click()
       await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('link', { name: 'https://reactpatterns.com/' })).toBeVisible()
 
+      // like button edits the entry and increments the likes
       await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'like' })).toBeVisible()
       await page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'like' }).click()
       await page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'like' }).click()
@@ -76,27 +78,63 @@ test.describe('Blog app', () => {
       await createBlog(page, 'React patterns', 'Michael Chan', 'https://reactpatterns.com/')
       await createBlog(page, 'Canonical string reduction', 'Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html')
 
+      // remove the blog
       await page.locator('.blog-summary').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'show' }).click()
       await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'remove' })).toBeVisible()
       page.on('dialog', dialog => dialog.accept())
       await page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'remove' }).click()
 
+      // make sure the removal confirmation appears and the blog is no longer in the list
       await expect(page.locator('.info').getByText('Blog deleted: React patterns')).toBeVisible()
       await expect(page.getByText('React patterns Michael Chan')).not.toBeVisible()
     })
 
     test('only the user who creates the blog can see the remove button', async ({ page }) => {
+      // create two blog entries with initial user
       await createBlog(page, 'React patterns', 'Michael Chan', 'https://reactpatterns.com/')
       await createBlog(page, 'Canonical string reduction', 'Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html')
 
+      // remove button is visible using the initial user 
       await page.locator('.blog-summary').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'show' }).click()
       await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'remove' })).toBeVisible()
 
+      // log out and log in with the second user
       await page.getByRole('button', { name: 'logout' }).click()
       await loginWith(page, "arnold", "second")
 
+      // remove button is no longer visible
       await page.locator('.blog-summary').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'show' }).click()
       await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'remove' })).not.toBeVisible()
+    })
+
+    test('blogs are arranged in the order according to the likes', async ({ page }) => {
+      await createBlog(page, 'React patterns', 'Michael Chan', 'https://reactpatterns.com/')
+      await createBlog(page, 'Canonical string reduction', 'Edsger W. Dijkstra', 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html')
+
+      // blogs initially displayed in creation order
+      await expect(page.locator('.blog-summary').first()).toContainText('React patterns Michael Chan')
+      await expect(page.locator('.blog-summary').nth(1)).toContainText('Canonical string reduction')
+
+      // expand both to show the like buttons
+      await page.locator('.blog-summary').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'show' }).click()
+      await page.locator('.blog-summary').filter({ hasText: 'Canonical string reduction' }).getByRole('button', { name: 'show' }).click()
+
+      // click twice on the second blog's like button. it moves to the first place in the list
+      await page.locator('.blog-details').filter({ hasText: 'Canonical string reduction' }).getByRole('button', { name: 'like' }).click()
+      await expect(page.locator('.blog-details').filter({ hasText: 'Canonical string reduction' }).getByText('likes: 1')).toBeVisible()
+      await page.locator('.blog-details').filter({ hasText: 'Canonical string reduction' }).getByRole('button', { name: 'like' }).click()
+      await expect(page.locator('.blog-details').filter({ hasText: 'Canonical string reduction' }).getByText('likes: 2')).toBeVisible()
+      await expect(page.locator('.blog-details').first()).toContainText('Canonical string reduction')
+
+      // click 3 times on the first blog's like button. it moves back to the first position 
+      await page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'like' }).click()
+      await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByText('likes: 1')).toBeVisible()
+      await page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'like' }).click()
+      await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByText('likes: 2')).toBeVisible()
+      await page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByRole('button', { name: 'like' }).click()
+      await expect(page.locator('.blog-details').filter({ hasText: 'React patterns Michael Chan' }).getByText('likes: 3')).toBeVisible()
+      await expect(page.locator('.blog-details').first()).toContainText('React patterns Michael Chan')
+
     })
 
   })
